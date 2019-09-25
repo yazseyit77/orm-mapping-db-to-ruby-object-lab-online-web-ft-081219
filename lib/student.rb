1dc52a3479ddc0651ceeccfc9e8ec42fdb552a92
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:50:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(x)
-    sql "SELECT * FROM Students WHERE grade = 10 LIMIT ?"
+    sql = "SELECT * FROM Students WHERE grade = 10 LIMIT ?"
 
     DB[:conn].execute(sql, x)
   end
