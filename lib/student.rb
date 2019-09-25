48bf3037e448a2764fbb3ae7cc7306cd4fcbf66f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:52:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
         WHERE grade < 12
       SQL
 
-    DB[:conn].execute(sql).map{|do| self.new_from_db(row)}
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
   end
 
   def self.first_X_students_in_grade_10(x)
