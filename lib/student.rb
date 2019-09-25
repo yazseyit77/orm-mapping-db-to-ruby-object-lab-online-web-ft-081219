4f78c6904cea278bb9a0339f305fc70fd0463241
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:35:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
         LIMIT 1
       SQL
 
-    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
   end
 
   def save
