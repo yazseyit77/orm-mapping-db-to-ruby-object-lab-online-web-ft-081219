0d337116d46d05bf7a8116fa4145daf2f809bf40
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:53:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,13 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 10
+        LIMIT 1
+      SQL
 
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
   end
 
   def save
