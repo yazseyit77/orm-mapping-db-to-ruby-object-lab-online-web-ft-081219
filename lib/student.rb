e71577e61e5e359866c1c4d553c1f6b3bcd65c54
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:55:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
         WHERE grade = ?
       SQL
 
-    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, x).map{|row| self.new_from_db(row)}
   end
 
   def save
