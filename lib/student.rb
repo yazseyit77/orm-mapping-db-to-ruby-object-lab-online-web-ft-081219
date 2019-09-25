443c04dea1a60af3bd92f16fdad80677de62e8e8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:43:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,10 +35,10 @@ class Student
   def self.all_students_in_grade_9
     sql = <<-SQL
         SELECT * FROM Students
-        WHERE grade = ?
+        WHERE grade = 9
       SQL
 
-    DB[:conn].execute(sql, grade).map{|row| self.new_from_db(row)}.first
+    DB[:conn].execute(sql)
   end
 
   def save
