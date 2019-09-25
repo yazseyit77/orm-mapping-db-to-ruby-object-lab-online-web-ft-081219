0203826c747c89560e46a4a69cf213725456b495
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-25 14:48:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
         SELECT * FROM Students
         WHERE grade = 10
